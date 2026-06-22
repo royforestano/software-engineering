7daaec9c8c9481e48e75027de432ccb0d318eecb
royforestano <royforestano@gmail.com>
2026-06-22 09:41:56 -0400

    Added interview prep questions.

---
+++ interview_prep/leet_code/36_valid_sodoku.py
@@ -0,0 +1,83 @@
+"""
+36. Valid Sudoku
+
+Difficulty: Medium
+Link:
+
+Determine if a 9 x 9 Sudoku board is valid. Only the filled cells need to be validated according to the following rules:
+
+Each row must contain the digits 1-9 without repetition.
+Each column must contain the digits 1-9 without repetition.
+Each of the nine 3 x 3 sub-boxes of the grid must contain the digits 1-9 without repetition.
+Note:
+
+A Sudoku board (partially filled) could be valid but is not necessarily solvable.
+Only the filled cells need to be validated according to the mentioned rules.
+ 
+
+Example 1:
+Input: board = 
+[["5","3",".",".","7",".",".",".","."]
+,["6",".",".","1","9","5",".",".","."]
+,[".","9","8",".",".",".",".","6","."]
+,["8",".",".",".","6",".",".",".","3"]
+,["4",".",".","8",".","3",".",".","1"]
+,["7",".",".",".","2",".",".",".","6"]
+,[".","6",".",".",".",".","2","8","."]
+,[".",".",".","4","1","9",".",".","5"]
+,[".",".",".",".","8",".",".","7","9"]]
+Output: true
+Example 2:
+
+Input: board = 
+[["8","3",".",".","7",".",".",".","."]
+,["6",".",".","1","9","5",".",".","."]
+,[".","9","8",".",".",".",".","6","."]
+,["8",".",".",".","6",".",".",".","3"]
+,["4",".",".","8",".","3",".",".","1"]
+,["7",".",".",".","2",".",".",".","6"]
+,[".","6",".",".",".",".","2","8","."]
+,[".",".",".","4","1","9",".",".","5"]
+,[".",".",".",".","8",".",".","7","9"]]
+Output: false
+Explanation: Same as Example 1, except with the 5 in the top left corner being modified to 8. Since there are two 8's in the top left 3x3 sub-box, it is invalid.
+ 
+
+Constraints:
+
+board.length == 9
+board[i].length == 9
+board[i][j] is a digit 1-9 or '.'.
+
+"""
+# N is the board height and length
+# Time: O(1) (or O(N^2) when scaling)
+# Space: O(1) (or O(N) when scaling)
+class Solution(object):
+    def isValidSudoku(self, board):
+        """
+        :type board: List[List[str]]
+        :rtype: bool
+        """
+        rows = {}
+        cols = {}
+        boxes = {}
+
+        for i in range(9):
+            for j in range(9):
+                val = board[i][j]
+                if val == '.':
+                    continue
+                    
+                box = (i // 3, j // 3)
+                rows.setdefault(i, set())
+                cols.setdefault(j, set())
+                boxes.setdefault(box, set())
+                if val in rows[i] or val in cols[j] or val in boxes[box]:
+                    return False
+
+                rows[i].add(val)
+                cols[j].add(val)
+                boxes[box].add(val)
+
+        return True
